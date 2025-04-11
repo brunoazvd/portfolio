2053bf7f0e258d2ba705be008703e7a91b5d783e
Bruno Azevedo <brunoazvd@protonmail.com>
2025-04-11 13:21:36 -0300

    fixed minor bug

--- src/projects/produtividade/produtividadeRoutes.js
+++ src/projects/produtividade/produtividadeRoutes.js
@@ -7,9 +7,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const router = Router();
+const produtividadeRoutes = Router();
 
-router.use("/", express.static(path.join(__dirname, "build")));
+produtividadeRoutes.use("/", express.static(path.join(__dirname, "build")));
 
 // Helper function to read/write JSON file
 async function readTasks() {
